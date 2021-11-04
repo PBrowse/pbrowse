b33d0929e01181235b705bc04845c11bfa68d602
PREKNOWLEDGEWEB <preknolwedge@gmail.com>
2021-11-04 16:58:47 +0530

    History language not loading from en.ts fixed#

--- README.md
+++ README.md
@@ -7,35 +7,13 @@
   <h1>PBrowse Browse Base</h1>
   <p>View News and More and Quick Links</p>
   <img src="https://www.pbrowse.ml/webres/shot-1.png" />
-<<<<<<< HEAD
-<<<<<<< HEAD
-  
-=======
 
->>>>>>> e5664d3 (Downloads can now be saced)
-=======
-
->>>>>>> dff6c4a6239762e903ce9a1e21965195aeb7c8e8
   ![workflow](https://github.com/PBrowse/pbrowse/actions/workflows/BuildApp.yml/badge.svg)
   
   <a href="https://github.com/cliqz-oss/adblocker/" target="_blank" rel="noopener noreferrer">
       <img alt="powered by Cliqz" src="https://img.shields.io/badge/cliqz-powered-blue?logo=cliqz">
   </a>
-<<<<<<< HEAD
-<<<<<<< HEAD
   
-  [![Discord](https://discordapp.com/api/guilds/875691715837689947/widget.png?style=shield)](https://discord.gg/DbrSU8637Y)
-  
-=======
-
-  [![Discord](https://discordapp.com/api/guilds/875691715837689947/widget.png?style=shield)](https://discord.gg/DbrSU8637Y)
-
->>>>>>> e5664d3 (Downloads can now be saced)
-=======
-
-  [![Discord](https://discordapp.com/api/guilds/875691715837689947/widget.png?style=shield)](https://discord.gg/DbrSU8637Y)
-
->>>>>>> dff6c4a6239762e903ce9a1e21965195aeb7c8e8
   <h3>Downloads ?</h3>
   are here <a href="https://www.pbrowse.ml/#downloads">Click Here</a>
 </div>  
--- src/renderer/views/history/components/App/index.tsx
+++ src/renderer/views/history/components/App/index.tsx
@@ -4,6 +4,7 @@ import { ipcRenderer } from 'electron';
 import store, { QuickRange } from '../../store';
 import { NavigationDrawer } from '~/renderer/components/NavigationDrawer';
 import { ThemeProvider } from 'styled-components';
+import * as Strings from '~/renderer/constants/trasnlations/en';
 import { SelectionDialog } from '~/renderer/components/SelectionDialog';
 import { HistorySection } from '../HistorySection';
 import { Container, Content, LeftContent } from '~/renderer/components/Pages';
@@ -92,23 +93,23 @@ export default observer(() => {
         <GlobalNavigationDrawer></GlobalNavigationDrawer>
         <NavigationDrawer title="History" search onSearchInput={onInput}>
           <RangeItem icon={ICON_ALL} range="all">
-            All
+            {Strings.All}
           </RangeItem>
           <RangeItem icon={ICON_TODAY} range="today">
-            Today
+          {Strings.Today}
           </RangeItem>
           <RangeItem icon={ICON_HISTORY} range="yesterday">
-            Yesterday
+          {Strings.yesterday}
           </RangeItem>
           <RangeItem icon={ICON_WEEK} range="last-week">
-            Last week
+          {Strings.last_week}
           </RangeItem>
           <RangeItem icon={ICON_CALENDAR} range="older">
-            Older
+          {Strings.older}
           </RangeItem>
           <div style={{ flex: 1 }} />
           <NavigationDrawer.Item icon={ICON_TRASH} onClick={onClearClick}>
-            Clear browsing data
+          {Strings.Clear_browsing_data}
           </NavigationDrawer.Item>
         </NavigationDrawer>
         <Content onScroll={onScroll}>
